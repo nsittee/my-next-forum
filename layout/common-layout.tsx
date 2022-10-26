e4e276ec2d3bfa3c48dca740c53b2364799f395e
nutthawut-itmx <nutthawut.sit@itmx.co.th>
2022-10-26 09:24:07 +0700

    Add MyHeader

--- layout/common-layout.tsx
+++ layout/common-layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { MyFooter } from './my-footer'
 import { MyHead } from './my-head'
+import { MyHeader } from './my-header'
 
 export const CommonLayout = (props: any) => {
   return (
     <div>
       <MyHead title={props.title ? props.title : "my-forum"} />
+      <MyHeader />
       {props.children}
       <MyFooter />
     </div>
---
+++ layout/my-header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MyHeader = (props: any) => {
+  return (
+    <div style={{
+      backgroundColor: "lightblue"
+    }}>
+      Header
+    </div>
+  )
+}
