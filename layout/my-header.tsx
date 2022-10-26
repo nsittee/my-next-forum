b13d75a7094c161f127e575122bfd6f0168fcc58
nutthawut-itmx <nutthawut.sit@itmx.co.th>
2022-10-26 11:18:42 +0700

    Add placeholder logic for header component

--- layout/my-header.tsx
+++ layout/my-header.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const MyHeader = (props: any) => {
+  const [authenticated, setAuthenticated] = useState(false)
+  const [username, setUsername] = useState<string>('')
+
   return (
     <div style={{
-      backgroundColor: "lightblue"
+      backgroundColor: "lightgreen",
     }}>
-      Header
+      <div style={{
+        margin: "0 50%"
+      }}>
+
+        {
+          !authenticated ?
+            <div>
+              <input onChange={(e) => setUsername(e.target.value)} />
+              <button onClick={() => {
+                setAuthenticated(true)
+              }}>
+                sign in
+              </button>
+            </div>
+            :
+            <div>
+              {username}
+              <button onClick={() => {
+                setAuthenticated(false)
+                setUsername('')
+              }}>
+                sign out
+              </button>
+            </div>
+        }
+      </div>
     </div>
   )
 }
