e6093cde51ad18a8bbf771accc9b54aa974152fc
nutthawut-itmx <nutthawut.sit@itmx.co.th>
2022-11-01 09:52:29 +0700

    Fix warning `legacy implementaion` for redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,18 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { wrapper } from '../store/store'
 import { MainLayout } from '../layout/main-layout'
+import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { store } = wrapper.useWrappedStore(pageProps);
+
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <Provider store={store}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </Provider>
   )
 }
 
-export default wrapper.withRedux(MyApp)
+export default MyApp
