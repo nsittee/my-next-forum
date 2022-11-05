777b82e822f55b4b97258d25ae5d212f855dd14b
bonbon.gaming <nsittee@gmail.com>
2022-11-05 08:47:46 +0700

    Add simple collapse for sidebar

--- layout/XSidebar.tsx
+++ layout/XSidebar.tsx
@@ -1,10 +1,11 @@
 import { Menu } from '@mui/icons-material';
-import { Drawer, Toolbar, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
-import React from 'react'
+import { Drawer, Toolbar, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Collapse } from '@mui/material'
+import React, { useState } from 'react'
 
 export const drawerWidth = 190;
 
 export const XSidebar = () => {
+  const [openMenu, setOpenMenu] = useState(false)
   return (
     <Drawer
       sx={{
@@ -25,26 +26,37 @@ export const XSidebar = () => {
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <Menu /> : <Menu />}
+                <Menu />
               </ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
           </ListItem>
         ))}
+        <Divider />
+
+        <ListItem key={'Others'} disablePadding>
+          <ListItemButton onClick={() => setOpenMenu(!openMenu)}>
+            <ListItemIcon>
+              <Menu />
+            </ListItemIcon>
+            <ListItemText primary={'Others'} />
+          </ListItemButton>
+        </ListItem>
       </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <Menu /> : <Menu />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <Collapse in={openMenu} timeout="auto" unmountOnExit>
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <Menu />
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Collapse>
     </Drawer>
   )
 }
