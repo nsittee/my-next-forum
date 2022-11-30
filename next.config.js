b2427cf60f623c836afc83cda29b7416a3791479
bonbon.gaming <nsittee@gmail.com>
2022-12-01 00:53:03 +0700

    Update next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   basePath: process.env.NEXT_PUBLIC_CONTEXT_PATH,
   reactStrictMode: true,
 }
