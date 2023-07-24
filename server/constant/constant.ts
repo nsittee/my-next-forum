27ec1f30c806587e050faae6bb4ef17a2472cb86
nutthawut-itmx <nutthawut.sit@itmx.co.th>
2023-07-24 14:56:37 +0700

    Refactor app constant

--- server/constant/constant.ts
+++ server/constant/constant.ts
@@ -4,8 +4,6 @@ const env = process.env
 
 // FIXME: usage of `!` symbol
 export const config = {
-  dbConnection: env.DB_CONNECTION!,
-
   secretKey: env.SECRET!,
 
   accessTokenDuration: env.ACCESS_TOKEN_DURATION!,
