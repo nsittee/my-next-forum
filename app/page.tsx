0e4c6ac4eb714a9fa7a7628ba1c588d0738e720c
nutthawut-itmx <nutthawut.sit@itmx.co.th>
2023-07-24 16:38:20 +0700

    Refactor folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+'use client'
 import { Metadata } from 'next'
+import { Provider } from 'react-redux'
+import { wrapper } from '../store/store'
+import { AppProps } from 'next/app'
 
 export const metadata: Metadata = {
   title: 'MyForum',
 }
 
-export default function Page() {
-  return <div>
-    Root Page
-  </div>
+export default function Page({ pageProps }: AppProps) {
+  return (
+    <Provider store={wrapper.useWrappedStore(pageProps).store}>
+      <div>
+        Root Page
+      </div>
+    </Provider>
+  )
 }
