5ab49fe6a458b139d7f175fec0241c8faf7e75b6
bonbon <nsittee@gmail.com>
2023-07-25 00:31:03 +0700

    no log

--- pages/api/v1/thread/all.ts
+++ pages/api/v1/thread/all.ts
@@ -5,7 +5,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  console.log("/api/v1/thread/all")
   // const subId = threadService.getSubFromId()
   const list = await threadService.getAll()
   res.status(200).json({
